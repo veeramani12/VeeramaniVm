ba9938070195ae61dc0bb9fe0f14690736f704f0
veeram1w <veeramani.ramaraj.ext@siemens.com>
2023-09-30 17:17:05 +0530

    New Project

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-// export all the components at once from here to App.js file
-
-
-export { default as Navbar } from './Navbar/Navbar';
-export { default as Header } from './Header/Header';
-export { default as Main } from './Main/Main';
-export { default as Footer } from './Footer/Footer';
-export { default as Reservations } from './Reservations/Reservations';
-export { default as OrderOnline } from './OrderOnline/OrderOnline';
